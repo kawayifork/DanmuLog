f09b4e685ed1c8de36644033f3271b01caedd1f0
kawayifork <68200530+kawayifork@users.noreply.github.com>
2020-08-25 13:04:03 +0800

    Initial commit

---
+++ SettingWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace DanmuLog
+{
+    /// <summary>
+    /// Interaction logic for SettingWindow.xaml
+    /// </summary>
+    public partial class SettingWindow : Window
+    {
+        public SettingWindow(PluginSettings Settings)
+        {
+            InitializeComponent();
+            this.DataContext = Settings;
+        }
+
+        internal void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
